075345d5753763cc46d270a5c3fe7bf6dc9f0b7a
caiomelotec <caio.melotec@icloud.com>
2023-05-08 10:55:11 +0200

    word counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputCouter, setInputCounter] = useState('')
+  function countWords(str) {
+    if (str.trim() === '') {
+      return 0;
+    }
+    let words = str.trim().split(/\s+/);
+
+    return words.length;
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Word Counter</h1>
+
+      <textarea className='input-text' type="text" value={inputCouter} onChange={(e) => setInputCounter(e.target.value)} />
+
+      <span className='counter-span'>Character: {inputCouter.length}</span>
+
+      <span className='counter-span'>Word: {countWords(inputCouter)}</span>
+
+      <button style={{ backgroundColor: inputCouter.length > 0 ? 'red' : 'grey', cursor: inputCouter.length > 0 ? 'pointer' : 'not-allowed' }} className='clear-btn' onClick={() => setInputCounter('')}>Clear</button>
+    </div >
   );
 }
 
